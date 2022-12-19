7285d74045cd80d94013c883f4db8eebf6273a7c
Tasha <ncarr63@gmail.com>
2022-12-19 21:08:06 +1300

    6:20 on  vue and axios kanye

---
+++ api-testing/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="app">
+  <h2>Hello</h2>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+new HelloWorld({
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://my-json-server.typicode.com/tasha53505/json-server-vue-project')
+      .then(response => (this.info = response))
+  }
+})
+</script>
